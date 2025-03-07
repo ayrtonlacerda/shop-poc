bda33fa985d6af910d3f46ef9d0f3e66668282cc
ayrtonlacerda <ayrtonrodrigues.lacerda@gmail.com>
2025-03-06 21:24:10 -0300

    esmodule import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import federation from "@originjs/vite-plugin-federation";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "app",
-      remotes: {
-        remoteApp: "http://localhost:3001/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
-});
-
-/*
 export default defineConfig({
   integrations: [
     react(),
@@ -32,4 +17,3 @@ export default defineConfig({
     },
   ],
 });
-*/
--- src/components/LikeButtonWrapper.jsx
+++ src/components/LikeButtonWrapper.jsx
@@ -1,6 +1,6 @@
-//import LikeButton from "http://localhost:3001/bundle.js";
+import LikeButton from "http://localhost:3001/bundle.js";
 //import LikeButton from "http://localhost:3000";
-import LikeButton from "http://localhost:3001/assets/remoteEntry.js";
+//import LikeButton from "http://localhost:3001/assets/remoteEntry.js";
 import React from "react";
 //import LikeButton from "remoteApp/LikeButton";
 
