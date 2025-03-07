39eccc2ccee72fe4229baefdf1c19ee3a1ea4998
ayrtonlacerda <ayrtonrodrigues.lacerda@gmail.com>
2025-03-07 09:16:16 -0300

    form vercel

--- src/components/LikeButtonWrapper.jsx
+++ src/components/LikeButtonWrapper.jsx
@@ -1,9 +1,7 @@
 //import LikeButton from "http://http://192.168.68.103:3001/bundle.js";
-import LikeButton from "https://unpkg.com/@ayrton.lacerda/likebutton@latest/dist/bundle.js";
-//import LikeButton from "http://localhost:3000";
-//import LikeButton from "http://localhost:3001/assets/remoteEntry.js";
+//import LikeButton from "https://unpkg.com/@ayrton.lacerda/likebutton@latest/dist/bundle.js";
+import LikeButton from "https://likebutton-beryl.vercel.app/bundle.js";
 import React from "react";
-//import LikeButton from "remoteApp/LikeButton";
 
 const LikeButtonWrapper = () => {
   return <LikeButton />;
